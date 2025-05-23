ccde7a4878ec3a1890063269338bdf0511d78dde
gvidocams <gaca6817@gmai.com>
2025-05-23 19:29:34 +0300

    Added initial solution template with the azure function for retrieving weather data

---
+++ WeatherForecast/src/ForecastRetriever/ForecastRetrieverFunction.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace ForecastRetriever;
+
+public class ForecastRetrieverFunction
+{
+    private readonly ILogger<ForecastRetrieverFunction> _logger;
+
+    public ForecastRetrieverFunction(ILogger<ForecastRetrieverFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("ForecastRetrieverFunction")]
+    public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        }
+    }
+}
