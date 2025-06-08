f5d59f4d310fe7f490abe3d291813a2282e3d151
gvidocams <gaca6817@gmai.com>
2025-06-08 09:59:03 +0300

    Renamed weather retrieval azure function

--- WeatherForecast/src/WeatherUpdater/WeatherUpdaterFunction.cs
+++ WeatherForecast/src/WeatherUpdater/WeatherUpdaterFunction.cs
@@ -4,23 +4,23 @@ using Microsoft.Extensions.Logging;
 
 namespace ForecastRetriever;
 
-public class ForecastRetrieverFunction(IWeatherUpdater weatherUpdater, ILogger<ForecastRetrieverFunction> logger)
+public class WeatherUpdaterFunction(IWeatherUpdater weatherUpdater, ILogger<WeatherUpdaterFunction> logger)
 {
     [Function("ForecastRetrieverFunction")]
     public async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
     {
         logger.LogInformation("{FunctionName} started executing at: {ExecutionStartTime}",
-            nameof(ForecastRetrieverFunction), DateTime.Now);
+            nameof(WeatherUpdaterFunction), DateTime.Now);
 
         await weatherUpdater.UpdateForTrackedCities();
 
         logger.LogInformation("{FunctionName} finished executing at: {ExecutionEndTime}",
-            nameof(ForecastRetrieverFunction), DateTime.Now);
+            nameof(WeatherUpdaterFunction), DateTime.Now);
 
         if (myTimer.ScheduleStatus is not null)
         {
             logger.LogInformation("Next {FunctionName} execution scheduled at: {NextExecutionStartTime}",
-                nameof(ForecastRetrieverFunction), myTimer.ScheduleStatus.Next);
+                nameof(WeatherUpdaterFunction), myTimer.ScheduleStatus.Next);
         }
     }
 }
